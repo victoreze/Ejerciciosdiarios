8e60d6ce8ff41c7c63d2b7d5f88a4c9def53b6b1
Víctor Ezequiel  Núñez <130800986+victoreze@users.noreply.github.com>
2024-04-21 18:45:21 +0000

    Corrección anagrama

--- anagrama.py
+++ anagrama.py
@@ -2,8 +2,8 @@ a= input("ingresa una palabra: ")
 b= input("ingresa otra palabra: ")
 
 def anagrama(a, b):
-  a= "".join(sorted(a))
-  b= "".join(sorted(b))
+  a= "".join(sorted(a.lower()))
+  b= "".join(sorted(b.lower()))
   if a == b:
     return "Es anagrama"
   return "No es anagrama"
