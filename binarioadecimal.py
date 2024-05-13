b959122d036b5e438bd15f621eb74a24443f1567
Víctor Ezequiel  Núñez <130800986+victoreze@users.noreply.github.com>
2024-05-13 22:09:32 +0000

    Add binario

---
+++ binarioadecimal.py
@@ -0,0 +1,10 @@
+# el numero binario se represnta '0b' y automáticamente se pasa a entero
+b = 0b101
+print(b)
+
+#la funcion 'int' y su base en binario que es 2
+a = int("101", 2)
+print(a)
+
+#funcion fstring 
+print(f"{0b101:#0}")
